814171e495e3a2e9249ab689f712a8d94c4d1d3c
Sam Wallace <samuelfrancis.wallace@uzh.ch>
2024-03-29 17:57:32 +0100

    Implemented check on PWD if not fufilled then BAD_REQUEST w/ Message thrown

---
+++ src/test/java/ch/uzh/ifi/hase/soprafs24/controller/PasswordValidatorTest.java
@@ -0,0 +1,49 @@
+import ch.uzh.ifi.hase.soprafs24.entity.User;
+import ch.uzh.ifi.hase.soprafs24.service.UserService;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.server.ResponseStatusException;
+
+import static org.mockito.Mockito.when;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+@ExtendWith(MockitoExtension.class)
+public class PasswordValidatorTest {
+
+    @Mock
+    User user;
+
+    @InjectMocks
+    UserService userService;
+
+    @Test
+    public void testValidPassword() {
+        when(user.getPassword()).thenReturn("Password123!");
+        userService.checkPassword(user); // Should not throw an exception
+    }
+
+    @Test
+    public void testInvalidPasswordMissingUppercase() {
+        when(user.getPassword()).thenReturn("password123!");
+        assertThrows(ResponseStatusException.class, () -> userService.checkPassword(user),
+                "The password doesn't contain an Upper Case Letter [A-Z]");
+    }
+
+    @Test
+    public void testInvalidPasswordMissingNumerical() {
+        when(user.getPassword()).thenReturn("Password!");
+        assertThrows(ResponseStatusException.class, () -> userService.checkPassword(user),
+                "The password doesn't contain a numerical character [0-9]");
+    }
+
+    @Test
+    public void testInvalidPasswordMissingSpecialChar() {
+        when(user.getPassword()).thenReturn("Password123");
+        assertThrows(ResponseStatusException.class, () -> userService.checkPassword(user),
+                "The password doesn't contain special Character.");
+    }
+}
