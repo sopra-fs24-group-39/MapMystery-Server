9a35762cc9e64967e4154cfbaae3901da0c8e664
Sam Wallace <samuelfrancis.wallace@uzh.ch>
2024-04-24 11:45:37 +0200

    Added more tests + Refactored Entities class

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Major.java
@@ -0,0 +1,10 @@
+package ch.uzh.ifi.hase.soprafs24.entity;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+
+@XmlAccessorType(XmlAccessType.FIELD)
+public class Major {
+    // Define fields according to XML structure
+    // You can add annotations if necessary
+}
--- src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Nearest.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/entity/Nearest.java
@@ -45,11 +45,109 @@ public class Nearest {
     @XmlElement
     private double distance;
 
+    // Getters
     public double getLatitude() {
         return latitude;
     }
 
     public double getLongitude() {
         return longitude;
+    }
+
+    public int getElevation() {
+        return elevation;
+    }
+
+    public String getTimezone() {
+        return timezone;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getProv() {
+        return prov;
+    }
+
+    public String getRegion() {
+        return region;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public double getInLatitude() {
+        return inLatitude;
+    }
+
+    public double getInLongitude() {
+        return inLongitude;
+    }
+
+    public String getAltgeocode() {
+        return altgeocode;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    // Setters
+    public void setLatitude(double latitude) {
+        this.latitude = latitude;
+    }
+
+    public void setLongitude(double longitude) {
+        this.longitude = longitude;
+    }
+
+    public void setElevation(int elevation) {
+        this.elevation = elevation;
+    }
+
+    public void setTimezone(String timezone) {
+        this.timezone = timezone;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setProv(String prov) {
+        this.prov = prov;
+    }
+
+    public void setRegion(String region) {
+        this.region = region;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public void setInLatitude(double inLatitude) {
+        this.inLatitude = inLatitude;
+    }
+
+    public void setInLongitude(double inLongitude) {
+        this.inLongitude = inLongitude;
+    }
+
+    public void setAltgeocode(String altgeocode) {
+        this.altgeocode = altgeocode;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
     }
 }
