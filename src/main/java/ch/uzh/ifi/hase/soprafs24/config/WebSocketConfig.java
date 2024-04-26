77a4d7abee3ba72be1875d8ea229ddf1ac5293f7
LiquidIce95 <davidsanchez062@gmail.com>
2024-04-26 18:38:20 +0200

    f

--- src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs24/config/WebSocketConfig.java
@@ -26,8 +26,8 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry registry) {
         // Configure the endpoint 6 enable CORS
-        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5500","http://sopra-fs24-group-39-client.oa.r.appspot.com:5500").withSockJS();
-        registry.addEndpoint("/wss").setAllowedOrigins("https://localhost:5500","https://sopra-fs24-group-39-client.oa.r.appspot.com:5500").withSockJS();
+        registry.addEndpoint("/ws").setAllowedOrigins("http://localhost:5500","http://localhost:3000","http://sopra-fs24-group-39-client.oa.r.appspot.com:5500","http://sopra-fs24-group-39-client.oa.r.appspot.com:3000").withSockJS();
+        registry.addEndpoint("/wss").setAllowedOrigins("https://localhost:5500","https://localhost:3000","https://sopra-fs24-group-39-client.oa.r.appspot.com:5500","https://sopra-fs24-group-39-client.oa.r.appspot.com:3000").withSockJS();
 
     }
 }
